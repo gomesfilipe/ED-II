6d05a1168f2e1c0abc87de4898be609d8dcffe96
gomesfilipe <filipejvmf@gmail.com>
2022-02-14 16:59:27 -0300

    Estrutura de pastas.

---
+++ busca-memoria-principal/client/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../include/io.h"
+#include "../include/str.h"
+#include "../include/suffix.h"
+
+int main(int argc, char** argv) {
+    
+    
+    return 0;
+}
