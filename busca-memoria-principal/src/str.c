d6ad9b4ee7afa060937442a9abbb35a0cfe094d5
gomesfilipe <filipejvmf@gmail.com>
2022-02-17 19:21:47 -0300

    .

--- busca-memoria-principal/src/str.c
+++ busca-memoria-principal/src/str.c
@@ -26,11 +26,12 @@ String* create_string(char *a) {
     // String *s = malloc(sizeof *s);
     String *s = (String*) malloc(sizeof(String));
     s->len = mystrlen(a); //6
-    s->c = malloc((s->len + 1) * sizeof(char)); //estavamos mallocando sem espaço pro \0
+    // s->c = malloc((s->len + 1) * sizeof(char)); //estavamos mallocando sem espaço pro \0
+    s->c = malloc((s->len) * sizeof(char)); //estavamos mallocando sem espaço pro \0
     for (int i = 0; i < s->len; i++) {
         s->c[i] = a[i];
     }
-    s->c[s->len] = '\0';
+    // s->c[s->len] = '\0';
     return s;
 }
 
